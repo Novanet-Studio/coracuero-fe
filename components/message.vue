fc754965f40585934f25912237832c3bab0182e8
David Arenas <davejs136@gmail.com>
2023-04-20 14:25:02 -0400

    add: message component

---
+++ components/message.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="wrapper">
+    <p class="message" :class="class">
+      {{ message }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+type Props = {
+  message: string;
+  halfMode?: boolean;
+  wrapperClass?: string;
+  class?: string;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  halfMode: false,
+});
+
+const wrapper = computed(() => {
+  const classes = props.wrapperClass;
+  const variant = props.halfMode ? 'message-wrapper-alt' : 'message-wrapper';
+
+  return `${classes} ${variant}`;
+});
+</script>
+
+<style scoped>
+.message-wrapper {
+  @apply w-full bg-color-8 p-6;
+}
+
+.message {
+  @apply text-color-1 text-[10px] text-center whitespace-pre-wrap leading-3;
+}
+
+.message-wrapper-alt {
+  @apply bg-color-8 p-6 rounded-md shadow w-[85%] mx-auto;
+}
+</style>
